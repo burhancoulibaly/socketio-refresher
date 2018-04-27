97f599520ec1bd3f9851beb25a5ee95405906ec5
burhancoulibaly <burhancoulibaly@gmail.com>
2018-04-27 12:02:18 -0400

    added support for nick name, and user is typing in the console, and showing whos online.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var path = require('path');
 
+
+var users = [];//holds each users username
+var connections = [];//holds the amount of users
+
 home = path.resolve("./frontend/html/home.html");
 html = path.resolve("./frontend/html");
 js = path.resolve("./frontend/js");
@@ -23,15 +27,38 @@ app.get('/', function(req,res){
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+    connections.push(socket);
+    console.log('A new user connected. %s user/users connected', connections.length);
 
     socket.on('chat message', function(msg){
         console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        socket.broadcast.emit('chat message', msg);
     });
 
-    socket.on('disconnect', function(){
-        console.log('a user disconnected');
+    socket.on('new user', function(usr){
+        socket.username = usr;
+        users.push(socket.username);
+        console.log('New user: ' + usr);
+        updateUsernames(socket);
+        socket.broadcast.emit('get users',users);
+        
+    })
+
+    socket.on('disconnect', function(usr){
+        socket.broadcast.emit('disconnected',socket.username);
+        console.log(socket.username+' disconnected. %s user/users connected', connections.length-1);  
+        users.splice(users.indexOf(socket.username),1); 
+        connections.splice(connections.indexOf(socket),1);
+        updateUsernames(socket);
+    })
+
+    socket.on('typing', function(user){
+        console.log(user+" is typing");
+        socket.broadcast.emit('isTyping',user);
     })
 });
+
+    function updateUsernames(socket){
+        io.emit('updateUsers',users);
+    }
 
--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,16 +1,50 @@
 var socket = io();
+var user;
 
 $(document).ready(function(){
-    var socket = io();
-
-    $('form').submit(function(){
-        socket.emit('chat message', $('#m').val());
+    $('#chat-home').submit(function(){
+        $('#messages').append('<li>You : '+$('#m').val());
+        socket.emit('chat message', user+": "+$('#m').val());
         $('#m').val('');
         return false;
     });
+
+    $('#usr-form').submit(function(){
+        user = $('#usr').val();
+        socket.emit('new user', $('#usr').val());
+        $('#usr').val('');
+        $('#chat-home').css("visibility","visible");
+        $('#usr-form').css("visibility","hidden");
+        return false;
+    })
+
+    $('#m').keypress(function(){
+        socket.emit('typing', user);
+    })
+
 })
 
 socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
+    $('#messages').append('<li>'+msg);
 })
 
+socket.on('get users', function(users){
+    $('#messages').append('<li> New user: '+users[users.length - 1]);
+})
+
+socket.on('isTyping',function(user){
+    console.log(user+" is Typing");
+})
+
+socket.on('disconnected',function(user){
+    // console.log("hello");
+    $('#messages').append('<li>'+user+" disconnected");
+})
+
+socket.on('updateUsers',function(users){
+    console.log(users);
+    $('#usrnames').html("");
+    users.forEach(function(user) {
+        $('#usrnames').append('<li>'+user);
+    });
+})
